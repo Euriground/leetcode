d5b230c6df938be5550a013b14e926ea5c666696
amirfazel <amirfazelk@systemgroup.net>
2026-09-21 17:49:15 +0330

    solved #1281

--- 1281.subtract the product and sum of digits of an integer/main.go
+++ 1281.subtract the product and sum of digits of an integer/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+)
 
 func main() {
 	fmt.Println(subtractProductAndSum(234))
@@ -9,5 +13,19 @@ func main() {
 // TODO: implement. See README.md for the problem statement.
 // https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 func subtractProductAndSum(n int) int {
-	return 0
+	stringify := strconv.Itoa(n)
+	nums := []int{}
+
+	for i := 0; i < utf8.RuneCountInString(stringify); i++ {
+		num, _ := strconv.Atoi(string(stringify[i]))
+		nums = append(nums, num)
+	}
+	sum := 0
+	product := 1
+
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		product *= nums[i]
+	}
+	return product - sum
 }
