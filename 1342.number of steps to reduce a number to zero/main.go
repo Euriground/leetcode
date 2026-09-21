06169bfe0a91808ab7520664246809faca9b9465
amirfazel <amirfazelk@systemgroup.net>
2026-09-21 17:10:11 +0330

    added comment

--- 1342.number of steps to reduce a number to zero/main.go
+++ 1342.number of steps to reduce a number to zero/main.go
@@ -8,6 +8,7 @@ func main() {
 
 // TODO: implement. See README.md for the problem statement.
 // https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+// random change
 func numberOfSteps(num int) int {
 	return 0
 }
